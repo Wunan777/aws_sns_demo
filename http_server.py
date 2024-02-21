f6313a76bad1e5724e6d46f9b01c889778481a77
Wunan777 <964721470@qq.com>
2024-02-22 00:59:37 +0800

    Update http_server.py

--- http_server.py
+++ http_server.py
@@ -5,14 +5,16 @@ app = Flask(__name__)
 
 
 @app.route("/sns/callback/confirmation", methods=["POST"])
-def handle_post():
+def callback_confirmation():
     # Get the data from the POST request
     try:
+
+        print(request)
         data = request.get_json()
         print(data)
         # Extract the SubscribeURL from the data
         subscribe_url = data.get("SubscribeURL")
-
+        print(subscribe_url)
         if subscribe_url:
             # Make a GET request to the SubscribeURL
             response = requests.get(subscribe_url)
@@ -24,8 +26,9 @@ def handle_post():
                 return "Failed to confirm subscription."
         else:
             return "SubscribeURL not found in the request data."
+
     except Exception as err:
-        print(err)
+        print("callback_confirmation err: {}".format(err))
 
 
 if __name__ == "__main__":
